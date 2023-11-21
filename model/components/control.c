e80b51c77086bc1461c1f0e453897ea8f2aeac65
Ulleren <ulrikbp@gmail.com>
2023-11-21 14:21:38 +0100

    Control signals done, except jal, jalr and ecall

--- model/components/control.c
+++ model/components/control.c
@@ -6,31 +6,60 @@ void updateControlSignals(Control *control) {
     control->memToReg = 0;
     control->branch = 0;
     control->aluSource = 0;
-    control->aluOp = 0;
+    control->aluOp = 0; // to be decided
     control->registerWrite = 0;
     // TODO: Less giant switch case
     switch (*control->input) {
         case 0x03: // Load instructions
+            control->aluSource = 1;
+            control->memToReg = 1;
+            control->registerWrite = 1;
+            control->memRead = 1;
+            // aluOp = add
             break;
         case 0x0F: // Fence instructions
+            // Not implemented
             break;
         case 0x13: // Immediate arithmetic instructions
+            control->aluSource = 1;
+            control->registerWrite = 1;
+            // aluOp = func3/7               control->aluOp = 0b10;
             break;
         case 0x17: // Add upper immediate to program counter
+            control->branch = 1;
             break;
         case 0x23: // Save instructions
+            control->aluSource = 1;
+            control->memWrite = 1;
+            // aluOp = add
             break;
         case 0x33: // Register arithmetic instructions
+            control->registerWrite = 1;
+            // aluUp = func3/7            control->aluOp = 0b10;
             break;
         case 0x37: // Load upper immediate
+            control->aluSource = 1;
+            control->registerWrite = 1;
+            // aluOp = add
             break;
         case 0x63: // Branch instructions
+            control->branch = 1;
+            // aluOp = func3/7
             break;
         case 0x67: // Jump and link register
+            control->aluSource = 1;
+            control->branch = 1;
+            control->registerWrite = 1;
+            // FIXME
             break;
         case 0x6F: // Jump and link
+            control->aluSource = 1;
+            control->branch = 1;
+            control->registerWrite = 1;
+            // FIXME
             break;
         case 0x73: // ecall, ebreak and CSRR.
+            // FIXME exit?
             break;
         default:
             printf("Invalid opcode: %x\n", *control->input);
